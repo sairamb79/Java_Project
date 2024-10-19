fd09a776f0bf4263a761a7609bfc9425f3183fd5
sairamb79 <12345678+yourusername@users.noreply.github.com>
2024-10-19 08:52:03 -0400

    Added Scannner feature

--- src/main/java/org/mycompany/webproject/Main.java
+++ src/main/java/org/mycompany/webproject/Main.java
@@ -1,5 +1,7 @@
 package org.mycompany.webproject;
 
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
@@ -9,6 +11,9 @@ public class Main {
         for (int i = 1; i <= 5; i++) {
             System.out.println("i = " + i);
             System.out.println("RAM IS WORKING ");
+
+            Scanner scanner = new Scanner (System.in);
+            System.out.println("Enter a number:");
         }
     }
 }
